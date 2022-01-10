acabda7e2bb86d80c8952652d73c1cbb3285a654
Doug Hagan <doug.hagan824@gmail.com>
2022-01-10 14:31:00 -0800

    more css styles

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+
+
+function Title() {
+    return (
+        <div>
+            <h1 className="Title" className="Neon">Lights Out!</h1>
+        </div>
+    )
+}
+
+
+
+export default Title
